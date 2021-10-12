a19239f04bbd745600e0d7f557089eeddb8178ed
Zaliy <83320921+Zaliy@users.noreply.github.com>
2021-10-12 17:53:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from Tools import Tools
 
 if __name__ == '__main__':
     ua = random.choice(Tools.read_yaml('settings.yaml')['USER_AGENT'])  # 获取配置文件ua
-    test = Test(client_ip='', proxy='', user_agent=ua, headless=None)
-    test.baidu()
+    test = Test(client_ip='', proxy='', user_agent=ua, headless=None)  # 创建流程实例化
+    test.baidu()  # 执行自动化流程
